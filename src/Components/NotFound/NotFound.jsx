6516ab6a8f98f37e3b67ac3da7d4d2d3432d6819
Mckreames <114367512+Mckreames@users.noreply.github.com>
2024-04-21 00:41:51 -0400

    fixed wording on Not Found page

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,41 +1,65 @@
-import React from 'react';
+import React from "react";
 import "./NotFound.css";
-import { Link } from 'react-router-dom';
-
-
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
+  const animateBubble = (x) => {
+    const wrapper = document.getElementById("bubble-wrapper");
+    const bubble = document.createElement("div");
 
-    const animateBubble = x => {
-        const wrapper = document.getElementById("bubble-wrapper");
-        const bubble = document.createElement("div");
-    
-        bubble.className = "bubble";
-    
-        bubble.style.left = `${x}px`;
-    
-        wrapper.appendChild(bubble);
-    
-        setTimeout(() => wrapper.removeChild(bubble), 2000);
-    }
-    
-    window.onmousemove = e => animateBubble(e.clientX);
+    bubble.className = "bubble";
+
+    bubble.style.left = `${x}px`;
+
+    wrapper.appendChild(bubble);
+
+    setTimeout(() => wrapper.removeChild(bubble), 2000);
+  };
+
+  window.onmousemove = (e) => animateBubble(e.clientX);
 
   return (
     <div className="sheet1">
-        <h1 className="pt-5 mt-2 mb-5 not-found-head">Uh-Oh... I hope we didn't break anything!</h1>
-        <h3 className="mb-0 not-found-text">Well this is embarassing 😅 This page no Longer exists!</h3>
-        <h3 className="pb-5 not-found-text">Need help finding your way? Maybe one of these solutions will work for you:</h3>
-        <ul className="offset-3 col-6 text-start">
-            <li className="pb-4">If a link sent you here, that may mean the link is no longer a working link.</li>
-            <li className="pb-4">If you've manually entered the URL, you may have gotten it wrong</li>
-            <li className="pb-4">Click <Link to="/" style={{ textDecoration: 'none' }}><a className="fancy-link">here</a></Link> to visit our homepage</li>
-            <li className="pb-4">If you don't know what Grace Note is, click <Link to="/about" style={{ textDecoration: 'none' }}><a className="fancy-link">here</a></Link> to find out all about our FREE gospel music app and all the features that come along with it!</li>
-            <li className="pb-4">If all else fails, our servers may be down 😣 Check back in a little later. We promise we won't take too long </li>
-            <li className="pb-4">Hey Zsolt! 👋🏽👋🏽👋🏽</li>
-        </ul>
-        <div id="bubble-wrapper">
-        </div>
+      <h1 className="pt-5 mt-2 mb-5 not-found-head">
+        Uh-Oh... I hope we didn't break anything!
+      </h1>
+      <h3 className="mb-0 not-found-text">
+        Well this is embarassing 😅 This page no Longer exists!
+      </h3>
+      <h3 className="pb-5 not-found-text">
+        Need help finding your way? Maybe one of these solutions will work for
+        you:
+      </h3>
+      <ul className="offset-3 col-6 text-start">
+        <li className="pb-4">
+          If a link sent you here, that may mean the link is no longer a working
+          link.
+        </li>
+        <li className="pb-4">
+          If you've manually entered the URL, you may have gotten it wrong
+        </li>
+        <li className="pb-4">
+          Click{" "}
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <a className="fancy-link">here</a>
+          </Link>{" "}
+          to visit our homepage
+        </li>
+        <li className="pb-4">
+          If you don't know what Grace Note is, click{" "}
+          <Link to="/about" style={{ textDecoration: "none" }}>
+            <a className="fancy-link">here</a>
+          </Link>{" "}
+          to learn all about our FREE gospel music app and all the features that
+          come along with it!
+        </li>
+        <li className="pb-4">
+          If all else fails, our servers may be down 😣 Check back on us a
+          little later. We promise we won't take too long{" "}
+        </li>
+        <li className="pb-4">Hey Zsolt! 👋🏽👋🏽👋🏽</li>
+      </ul>
+      <div id="bubble-wrapper"></div>
     </div>
   );
 }
